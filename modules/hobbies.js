6df130ca531afbc919bc136af46d4f6ea8802fb0
marcopriests <prtmrc98@gmail.com>
2025-04-14 14:59:23 +0200

    [UPD] 2 - done

---
+++ modules/hobbies.js
@@ -0,0 +1,7 @@
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+    return [hobbyOne, hobbyTwo, hobbyThree]
+}
+
+console.log(hobbies('lettura', 'scacchi', 'giochi da tavolo'))
+
+module.exports = hobbies
